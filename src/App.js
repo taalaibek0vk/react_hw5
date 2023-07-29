00eedf370309da935d87582d04f1aad9371a9614
taalaibek0vk <taalaibekov1503@gmail.com>
2023-07-29 11:57:43 +0600

    сделал ДЗ5

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { createContext, useContext, useState } from 'react';
+
+const ThemeContext = createContext(null);
+
+export default function MyApp() {
+  const [theme, setTheme] = useState('light');
+  return (
+    <ThemeContext.Provider value={theme}>
+      <Form />
+      <label>
+        <input
+          type="checkbox"
+          checked={theme === 'dark'}
+          onChange={(e) => {
+            setTheme(e.target.checked ? 'dark' : 'light')
+          }}
+        />
+        Темный режим
+      </label>
+    </ThemeContext.Provider>
+  )
+}
+
+function Form({ children }) {
+  return (
+    <Panel title="Kairat Taalaibekov">
+      <h3>15.03.2000</h3>
+      <h4>Bishkek, Kyrgyzstan</h4>
+    </Panel>
+  );
+}
+
+function Panel({ title, children }) {
+  const theme = useContext(ThemeContext);
+  const className = 'panel-' + theme;
+  return (
+    <section className={className}>
+      <h1>{title}</h1>
+      {children}
+    </section>
+  )
+}
